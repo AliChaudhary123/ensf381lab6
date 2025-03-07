05420bb51b6006e8f4cd6a1d8cb5ed4f99dc0122
sim <simarkandola51@gmail.com>
2025-03-07 11:25:29 -0700

    3

--- jsx-exercise/src/App.js
+++ jsx-exercise/src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
+import EngineeringTopic from "./EngineeringTopics";
 
 function App() {
   const date = new Date();
   const currentDate = date.toLocaleDateString();
   let isLoggedIn = true;
   return (
-    <div className="App">
-      <h1>ENSF-381: Full Stack Web Development</h1>
-      <p>React Components</p>
-      <p>{currentDate}</p>
-      {isLoggedIn ? <p>Welcome back!</p> : <p>Please log in.</p>}
-    </div>
+    <>
+      <div className="App">
+        <h1>ENSF-381: Full Stack Web Development</h1>
+        <p>React Components</p>
+        <p>{currentDate}</p>
+        {isLoggedIn ? <p>Welcome back!</p> : <p>Please log in.</p>}
+      </div>
+      <EngineeringTopic />
+    </>
   );
 }
 
